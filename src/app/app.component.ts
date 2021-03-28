00165b129442990529108118f978fe0ee66743c0
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-28 21:03:58 +0300

    custom pipe done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   date: string;
   amount: number;
   height: number;
+  miles: number;
 
   car = {
     make: 'Toyota',
@@ -31,5 +32,9 @@ export class AppComponent {
 
   onHeightChange(value: string) {
     this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
   }
 }
